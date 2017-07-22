1424d005b5d42fe29988f835444a7ceb97056905
Valentyn Koval <koval.valentyn.pavlovich@gmail.com>
2017-07-22 15:42:31 +0300

    add function

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,86 @@
 var path = require('path');
 var webpack = require('webpack');
-
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:3000',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './src/index.js',
+    // the entry point of our app
   ],
+
   output: {
-    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+    // the output bundle
+
+    path: path.resolve(__dirname, 'dist'),
+
     publicPath: '/static/'
+    // necessary for HMR to know where to load the hot update chunks
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+
+  devtool: 'inline-source-map',
+
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+            use: [
+                  {
+                    loader: "css-loader" // translates CSS into CommonJS
+                  },
+                  {
+                    loader: "sass-loader" // compiles Sass to CSS
+                  }
+            ],
+             fallback: "style-loader" // used when css not extracted
+        }))
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: 'url-loader'
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new ExtractTextPlugin({filename: 'styles.css', allChunks: true}),
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+  ],
+
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+
+    historyApiFallback: true,
+    // respond to 404s with index.html
+
+    hot: true,
+    // enable HMR on the server
+  },
 };
